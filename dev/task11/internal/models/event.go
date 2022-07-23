46944bd96a1738e2272e2e313ae0670e323a7b42
Maxim Partin <max.physmath.05@gmail.com>
2022-07-23 17:12:45 +0300

    update model

--- dev/task11/internal/models/event.go
+++ dev/task11/internal/models/event.go
@@ -1,8 +1,11 @@
 package models
 
 type Event struct {
-	UserID       string `json:"user_id"`
+	UserID       int    `json:"user_id"`
+	EventID      int    `json:"event_id"`
+	Title        string `json:"title"`
 	DateCreated  string `json:"date_created"`
 	DateFinished string `json:"date_finished"`
 	Description  string `json:"description"`
+	Done         bool   `json:"done"`
 }
